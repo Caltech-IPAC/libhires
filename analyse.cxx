306728b175a036bd62c8f7998f863be5fdef1cd2
Walter Landry <wlandry@caltech.edu>
2013-01-10 13:04:09 -0800

    Add test and analyse script

---
+++ analyse.cxx
@@ -0,0 +1,22 @@
+#include <CCfits>
+
+int main(int argc, char *argv[])
+{
+  if(argc!=3)
+    {
+      std::cerr << "Need exactly two arguments\n";
+      exit(1);
+    }
+  CCfits::FITS py(argv[1]);
+  CCfits::FITS cxx(argv[2]);
+
+  std::valarray<double> py_image,cxx_image;
+  py.pHDU().read(py_image);
+  cxx.pHDU().read(cxx_image);
+
+  std::valarray<double> diff(py_image-cxx_image);
+
+  std::cout << std::abs(py_image+cxx_image).max()/2 << " "
+            << std::abs(diff).max() << " "
+            << std::abs(diff/(py_image+cxx_image+abs(cxx_image).max()*1e-30)).max() << "\n";
+}
