715917f84981e6b8321ab07d72216740643aeba0
Walter Landry <wlandry@caltech.edu>
2013-01-11 15:10:27 -0800

    Put Sample into the hires namespace.

--- src/cxx/Sample.hxx
+++ src/cxx/Sample.hxx
@@ -4,19 +4,23 @@
 #include <valarray>
 #include <utility>
 
-class Sample
+namespace hires
 {
-public:
-  std::valarray<double> x,y,flux,angle;
-  int id;
+  class Sample
+  {
+  public:
+    std::valarray<double> x,y,flux,angle;
+    int id;
 
-  Sample(const std::valarray<double> &X, const std::valarray<double> &Y,
-         const std::valarray<double> &Flux, const int &Id,
-         const std::valarray<double> &Angle): x(X), y(Y), flux(Flux),
-                                              angle(Angle), id(Id) {}
-  Sample(const std::valarray<double> &X, const std::valarray<double> &Y,
-         const std::valarray<double> &Flux, const int &Id):
-    Sample(X,Y,Flux,Id,std::valarray<double>()) {}
-};
+    Sample(const std::valarray<double> &X, const std::valarray<double> &Y,
+           const std::valarray<double> &Flux, const int &Id,
+           const std::valarray<double> &Angle): x(X), y(Y), flux(Flux),
+                                                angle(Angle), id(Id) {}
+    Sample(const std::valarray<double> &X, const std::valarray<double> &Y,
+           const std::valarray<double> &Flux, const int &Id):
+      Sample(X,Y,Flux,Id,std::valarray<double>()) {}
+    Sample() {}
+  };
+}
 
 #endif
