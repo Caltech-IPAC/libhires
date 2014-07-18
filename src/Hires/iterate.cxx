84a1ebc9860159a495e5fa40e0c1b86b2da9cc62
Walter Landry <wlandry@caltech.edu>
2014-07-17 17:26:16 -0700

    Fix an off-by-one error

--- src/Hires/iterate.cxx
+++ src/Hires/iterate.cxx
@@ -18,15 +18,15 @@ void Hires::iterate (std::vector<Sample> &samples)
   // Standard hires/minimap processing - always happens.
   //
   arma::mat correction, correction_squared;
-  footprints.compute_correction (ni, nj, flux_images[iteration - 1], iteration, 1,
+  footprints.compute_correction (ni, nj, flux_images[iteration], iteration+1, 1,
                                  boost_func, boost_max_iter, correction,
                                  correction_squared);
   correction /= wgt_image;
-  flux_images[iteration] = flux_images[iteration - 1] % correction; // Schur product
+  flux_images[iteration+1] = flux_images[iteration] % correction; // Schur product
 
   arma::mat corr_sq_image = (correction_squared / wgt_image)
     - square (correction);
-  cfv_images[iteration] = corr_sq_image;
+  cfv_images[iteration+1] = corr_sq_image;
 
   //
   // Optional HIRES beam generation
@@ -37,10 +37,10 @@ void Hires::iterate (std::vector<Sample> &samples)
       footprints.set_fluxes_to_sim_values (spike_image ());
 
       arma::mat correction, correction_squared;
-      footprints.compute_correction (ni, nj, beam_images[iteration - 1], iteration,
+      footprints.compute_correction (ni, nj, beam_images[iteration], iteration+1,
                                      false, boost_func, boost_max_iter,
                                      correction, correction_squared);
-      beam_images[iteration] = beam_images[iteration - 1] % correction
+      beam_images[iteration+1] = beam_images[iteration] % correction
         / wgt_image; // Schur product
     }
   ++iteration;
