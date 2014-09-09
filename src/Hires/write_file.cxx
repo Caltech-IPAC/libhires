7c41412a7dca864e1235a34fb681e31094106cc5
Walter Landry <wlandry@caltech.edu>
2014-09-09 15:16:32 -0700

    Cleanup

--- src/Hires/write_file.cxx
+++ src/Hires/write_file.cxx
@@ -8,23 +8,14 @@ namespace hires
 void Hires::write_file (arma::mat image, std::string filename,
                         const char *desc, int iter, int isflux)
 {
-  std::stringstream istr;
-  istr << iter;
-  std::string kwd, val, comment;
   std::vector<std::tuple<std::string, std::string, std::string> >
   file_specific_keywords;
 
-  file_specific_keywords.clear ();
+  file_specific_keywords.push_back
+    (std::make_tuple ("ITERNUM", std::to_string(iter),
+                      "HIRES iteration number"));
 
-  kwd = "ITERNUM";
-  val = istr.str ();
-  comment = "HIRES iteration number";
-  file_specific_keywords.push_back (std::make_tuple (kwd, val, comment));
-
-  kwd = "FILETYPE";
-  val = desc;
-  comment = "";
-  file_specific_keywords.push_back (std::make_tuple (kwd, val, comment));
+  file_specific_keywords.push_back (std::make_tuple ("FILETYPE", desc, ""));
 
   if (iter!=0)
     file_specific_keywords.push_back
