c9625c92c858c8538583f52f816fb6be1b1bcec0
Walter Landry <wlandry@caltech.edu>
2014-09-09 15:21:33 -0700

    Only output ITERNUM if it is not minimap

--- src/Hires/write_file.cxx
+++ src/Hires/write_file.cxx
@@ -11,25 +11,23 @@ void Hires::write_file (arma::mat image, std::string filename,
   std::vector<std::tuple<std::string, std::string, std::string> >
   file_specific_keywords;
 
-  file_specific_keywords.push_back
-    (std::make_tuple ("ITERNUM", std::to_string(iter),
-                      "HIRES iteration number"));
-
   file_specific_keywords.push_back (std::make_tuple ("FILETYPE", desc, ""));
 
   if (iter!=0)
-    file_specific_keywords.push_back
-      (std::make_tuple ("DRF_IN",
-                        boost::filesystem::path (drf_prefix).filename ().string ()
-                        + "*",
-                        "Name of Detector Response Files"));
-  if (isflux)
     {
-      kwd = "BUNIT";
-      val = flux_units;
-      comment = "";
-      file_specific_keywords.push_back (std::make_tuple (kwd, val, comment));
+      file_specific_keywords.push_back
+        (std::make_tuple ("ITERNUM", std::to_string(iter),
+                          "HIRES iteration number"));
+
+      file_specific_keywords.push_back
+        (std::make_tuple ("DRF_IN",
+                          boost::filesystem::path (drf_prefix).filename ().string ()
+                          + "*",
+                          "Name of Detector Response Files"));
     }
+
+  if (isflux)
+    file_specific_keywords.push_back (std::make_tuple ("BUNIT", flux_units, ""));
 
   write_fits (image, file_specific_keywords, filename);
 }
