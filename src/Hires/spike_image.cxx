b216ad251e749403fe36488834ad8f77187c05e5
Walter Landry <wlandry@caltech.edu>
2014-10-19 01:13:25 -0700

    Fix an infinite loop in spike_image when the image is too small for spikes.

--- src/Hires/spike_image.cxx
+++ src/Hires/spike_image.cxx
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <armadillo>
 #include "../Hires.hxx"
 
@@ -7,7 +8,8 @@ arma::mat Hires::spike_image ()
 {
   arma::mat spike (nxy[1], nxy[0]);
   spike.fill (0.000001);
-  const int i_delta (nxy[0] / beam_spike_n), j_delta (nxy[1] / beam_spike_n);
+  const int i_delta (std::max(nxy[0] / beam_spike_n,1)),
+    j_delta (std::max(nxy[1] / beam_spike_n,1));
   for (int i = i_delta / 2; i < nxy[0]; i += i_delta)
     for (int j = j_delta / 2; j < nxy[1]; j += j_delta)
       spike (j, i) = beam_spike_height;
